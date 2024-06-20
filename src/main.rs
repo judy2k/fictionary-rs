d3167ea60df07c3fe406982c17ac356c870e1174
Mark Smith <judy@judy.co.uk>
2024-06-20 11:40:54 +0100

    Load wordlist from user's data directory.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod model;
 
-use std::{io, fs, io::BufRead};
+use std::{fs, io::{self, BufRead}, path::{Path, PathBuf}};
 use clap::Parser;
 
+use directories::ProjectDirs;
 use model::{CharCounter, CharkovChain};
 
 #[derive(Parser, Debug)]
@@ -21,16 +22,21 @@ struct Args {
 fn main() -> eyre::Result<()> {
     let args = Args::parse();
 
-    let filepath = "ispell_wordlist/english.0";
-    println!("Reading {filepath}");
-    let charkov = load_wordfile(filepath)?;
+    let mut filepath: PathBuf = "ispell_wordlist/english.0".into();
+    if let Some(project_dirs) = ProjectDirs::from("uk.co", "judy", "fictionary") {
+        filepath = project_dirs.data_dir().to_owned();
+        filepath.push("english.0");
+    }
+    
+    println!("Reading {filepath:?}");
+    let charkov = load_wordfile(&filepath)?;
     for _ in 0..args.count {
         println!("{}", charkov.word(args.min_length, args.max_length)?);
     }
     Ok(())
 }
 
-fn load_wordfile(path: &str) -> io::Result<CharkovChain> {
+fn load_wordfile(path: &Path) -> io::Result<CharkovChain> {
     let buf = io::BufReader::new(fs::File::open(path)?).lines();
     let mut result = CharCounter::new();
     for word in buf.map_while(Result::ok) {
