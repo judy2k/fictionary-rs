baec77ba2ab260a89b54bf2ef7606f82a9f8b9aa
Judy2k <judy@judy.co.uk>
2024-06-21 11:45:52 +0100

    Rename project from fictionary-rs.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use clap::{Args, Parser, Subcommand};
 use std::{
     fs,
     io::{self, BufRead, Read, Write},
-    path::{Path, PathBuf},
+
 };
 
+use camino::{Utf8Path, Utf8PathBuf};
 use directories::ProjectDirs;
+use eyre::{Result, eyre, WrapErr};
 use model::{CharCounter, CharkovChain};
 use postcard::{from_bytes, to_allocvec};
 use thiserror::{self, Error};
@@ -33,20 +35,20 @@ struct WordsArgs {
     #[arg(short = 'x', long, default_value_t = 10)]
     max_length: usize,
     #[arg(short = 'f', long)]
-    fictionary_file: Option<PathBuf>,
+    fictionary_file: Option<Utf8PathBuf>,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
     Words(WordsArgs),
     Compile {
-        wordlist_path: PathBuf,
-        output_path: PathBuf,
+        wordlist_path: Utf8PathBuf,
+        output_path: Utf8PathBuf,
     },
     DataDir,
 }
 
-fn main() -> eyre::Result<()> {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
     match &args.command {
@@ -55,19 +57,19 @@ fn main() -> eyre::Result<()> {
             Commands::Compile {
                 wordlist_path,
                 output_path,
-            } => command_compile(&wordlist_path as &Path, &output_path),
+            } => command_compile(&wordlist_path as &Utf8Path, &output_path),
             Commands::DataDir => command_datadir(&args),
         },
         None => command_words(&args, &args.words),
     }
 }
 
-fn command_words(_args: &Cli, words_args: &WordsArgs) -> eyre::Result<()> {
-    let mut filepath: PathBuf = "./american.fictionary".into();
+fn command_words(_args: &Cli, words_args: &WordsArgs) -> Result<()> {
+    let mut filepath: Utf8PathBuf = "./american.fictionary".into();
     if let Some(ref provided_path) = words_args.fictionary_file {
         filepath.clone_from(provided_path);
     } else if let Some(project_dirs) = ProjectDirs::from("uk.co", "judy", "fictionary") {
-        project_dirs.data_dir().clone_into(&mut filepath);
+        filepath = Utf8PathBuf::from_path_buf(project_dirs.data_dir().into()).map_err(|p| eyre!(p.to_string_lossy().to_string())).wrap_err("Only UTF-8 file paths are supported.")?;
         filepath.push("american.fictionary");
     }
 
@@ -82,19 +84,19 @@ fn command_words(_args: &Cli, words_args: &WordsArgs) -> eyre::Result<()> {
     Ok(())
 }
 
-fn command_compile(wordlist_path: &Path, output_path: &Path) -> eyre::Result<()> {
+fn command_compile(wordlist_path: &Utf8Path, output_path: &Utf8Path) -> Result<()> {
     save_charkov(&load_wordfile(wordlist_path)?, output_path)?;
     Ok(())
 }
 
-fn command_datadir(_args: &Cli) -> eyre::Result<()> {
+fn command_datadir(_args: &Cli) -> Result<()> {
     if let Some(project_dirs) = ProjectDirs::from("uk.co", "judy", "fictionary") {
         println!("{}", project_dirs.data_dir().to_string_lossy());
     }
     Ok(())
 }
 
-fn save_charkov(charkov: &CharkovChain, path: impl AsRef<Path>) -> io::Result<()> {
+fn save_charkov(charkov: &CharkovChain, path: impl AsRef<Utf8Path>) -> io::Result<()> {
     let path = path.as_ref();
     let mut f = fs::File::create(path)?;
     let buf: Vec<u8> = to_allocvec(&charkov).unwrap();
@@ -116,16 +118,16 @@ enum CharkovFileError {
     },
 }
 
-fn load_charkov(path: impl AsRef<Path>) -> Result<CharkovChain, CharkovFileError> {
-    let mut reader = io::BufReader::new(fs::File::open(path)?);
+fn load_charkov(path: impl AsRef<Utf8Path>) -> Result<CharkovChain, CharkovFileError> {
+    let mut reader = io::BufReader::new(fs::File::open(path.as_ref())?);
 
     let mut buf = vec![];
     reader.read_to_end(&mut buf)?;
     Ok(from_bytes(&buf)?)
 }
 
-fn load_wordfile(path: impl AsRef<Path>) -> io::Result<CharkovChain> {
-    let buf = io::BufReader::new(fs::File::open(path)?).lines();
+fn load_wordfile(path: impl AsRef<Utf8Path>) -> io::Result<CharkovChain> {
+    let buf = io::BufReader::new(fs::File::open(path.as_ref())?).lines();
     let mut result = CharCounter::new();
     for word in buf.map_while(Result::ok) {
         if !word.starts_with(|c: char| c.is_uppercase()) && !word.contains('\'') {
